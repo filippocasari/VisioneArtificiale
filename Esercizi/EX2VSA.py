4531bdac73b75ba9420312642aaead3c718375d3
filippocasari <256034@studenti.unimore.it>
2020-10-15 17:59:43 +0200

    add ex2

---
+++ Esercizi/EX2VSA.py
@@ -0,0 +1,27 @@
+import random
+import numpy as np
+from skimage import data
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+
+
+im = data.astronaut()
+im = np.swapaxes(np.swapaxes(im, 0, 2), 1, 2)
+nbin = random.randint(32,128)
+
+
+import numpy as np
+
+H=im.shape[1]
+W=im.shape[2]
+
+out=np.zeros((3*nbin,))
+for color in range(3):
+     for x in range(H):
+         for y in range(W):
+             b= (im[color][x][y]*nbin) // 256
+             out[b+nbin*color]+=1
+
+out=out/sum(out)
+print(out)
